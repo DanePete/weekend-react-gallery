272aa515c0dd273bbbd78fe044fe951c8224b641
dane <dane.m.petersen@gmail.com>
2021-08-14 20:34:47 -0500

    improved hover

--- database.sql
+++ database.sql
@@ -5,4 +5,8 @@ CREATE TABLE gallery (
     likes integer
 );
 
-INSERT INTO "public"."gallery"("id", "path", "description", "likes") VALUES(4, 'images/goat_small.jpg', 'Photo of a goat taken at Glacier National Park21231', 13) RETURNING "id", "path", "description", "likes";
+INSERT INTO "public"."gallery"("id", "path", "description", "likes") VALUES(2, 'images/1.jpg', 'You will remember to wash your hands, won''t you?', 2) RETURNING "id", "path", "description", "likes";
+INSERT INTO "public"."gallery"("id", "path", "description", "likes") VALUES(3, 'images/2.jpg', 'You''re a fasinating creature?', 10) RETURNING "id", "path", "description", "likes";
+INSERT INTO "public"."gallery"("id", "path", "description", "likes") VALUES(4, 'images/3.jpg', 'Beach Day', 5) RETURNING "id", "path", "description", "likes";
+INSERT INTO "public"."gallery"("id", "path", "description", "likes") VALUES(5, 'images/4.jpg', 'I wonder what the trees are thinking', 19) RETURNING "id", "path", "description", "likes";
+INSERT INTO "public"."gallery"("id", "path", "description", "likes") VALUES(6, 'images/5.jpg', 'You''d think they''d fix this..', 55) RETURNING "id", "path", "description", "likes";
